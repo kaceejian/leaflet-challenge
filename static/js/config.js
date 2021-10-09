f026697afe2fe04ba323920fb4f970856553c485
kc chien <kaceejian@gmail.com>
2021-10-09 16:17:48 -0700

    upddate files

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,19 @@
-// API key
-const API_KEY = "YOUR API KEY HERE!";
+const baseUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?";
+
+const earthquakes = [];
+
+fetch(
+  `${baseUrl}format=geojson&starttime=2021-01-01&endtime=2021-10-09&minlatitude=30&maxlatitude=50&minlongitude=110&maxlongitude=130`
+)
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+    earthquakes.push(data.features);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+console.log(earthquakes);
