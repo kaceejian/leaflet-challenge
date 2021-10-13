20a47084528c7048b64abb2004af6d50cd8e7642
kc chien <kaceejian@gmail.com>
2021-10-13 08:01:51 -0700

    update files

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,5 @@
 const baseUrl =
   "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+const mapBoxKey =
+  "pk.eyJ1Ijoia2NqaWFuIiwiYSI6ImNrdXBtZmJibjRtc3YzMnE2Zml6ZWQ5YWUifQ.CfXh_clp30NG12AGgGtoRw";
--- static/js/logic.js
+++ static/js/logic.js
@@ -11,5 +11,30 @@ fetch(baseUrl)
 
 function draw(earthquakes) {
   console.log(earthquakes);
-  var myMap = L.map("map").setView([51.505, -0.09], 13);
+  var myMap = L.map("map").setView([51.505, -0.09], 1.75);
+  L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: "mapbox/streets-v11",
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: mapBoxKey,
+    }
+  ).addTo(myMap);
+
+  for (var earthquake of earthquakes) {
+    //   draw a dot
+    var circle = L.circle(
+      [earthquake.geometry.coordinates[1], earthquake.geometry.coordinates[0]],
+      {
+        color: "red",
+        fillColor: "#f03",
+        fillOpacity: 0.5,
+        radius: 5000 * earthquake.properties.mag,
+      }
+    ).addTo(myMap);
+  }
 }
